9a2a1369984ee9f9433d597d7a96a5fb0ad5a8d6
azmammad.q <azmammad.q@kripton.az>
2022-09-16 15:38:58 +0400

    register methods added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
-  console.log(req.session);
   res.render("auth/login", {
     pageTitle: "Login Page",
     path: "/login",
@@ -19,6 +20,39 @@ exports.postLogin = (req, res, next) => {
       });
     })
     .catch((err) => console.log(err));
+};
+
+exports.getRegister = (req, res, next) => {
+  res.render("auth/register", {
+    pageTitle: "Register Page",
+    path: "/register",
+    isAuthenticated: req.session.user,
+  });
+};
+
+exports.postRegister = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/redirect");
+      }
+      return bcrypt.hash(password, 12).then((password) => {
+        const user = new User({
+          email: email,
+          password: password,
+          cart: { items: [] },
+        });
+        return user.save();
+      });
+    })
+    .then((result) => {
+      return res.redirect("login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const authController = require("../controllers/auth");
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
+router.get("/register", authController.getRegister);
+router.post("/register", authController.postRegister);
 router.post("/logout", authController.postLogout);
 
 module.exports = router;
