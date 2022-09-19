5f172253a5308d411dfaaef54369bc54cc9d873b
azmammad.q <azmammad.q@kripton.az>
2022-09-19 13:20:27 +0400

    authorization added to admin actions

--- controllers/admin.js
+++ controllers/admin.js
@@ -61,21 +61,23 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then((product) => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect("/");
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.imageUrl = updatedImageUrl;
       product.description = updatedDesc;
-      return product.save();
-    })
-    .then((result) => {
-      console.log("UPDATED PRODUCT!");
-      res.redirect("/admin/products");
+      return product.save().then((result) => {
+        console.log("UPDATED PRODUCT!");
+        res.redirect("/admin/products");
+      });
     })
     .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({userId: req.user.id})
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -88,7 +90,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user.id })
     .then(() => {
       console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
