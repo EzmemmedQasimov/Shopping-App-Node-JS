33e453b344e7c4fcd2cb4475979d996db27eb27b
azmammad.q <azmammad.q@kripton.az>
2022-09-19 12:52:16 +0400

    password reset is done

--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,9 @@ router.post("/login", authController.postLogin);
 router.get("/register", authController.getRegister);
 router.post("/register", authController.postRegister);
 router.post("/logout", authController.postLogout);
+router.get("/reset", authController.getReset);
+router.post("/reset", authController.postReset);
+router.get("/reset/:token", authController.getNewPasswrod);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
