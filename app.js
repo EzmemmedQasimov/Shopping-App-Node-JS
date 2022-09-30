2e9d250b4d4bb403791c378e67eaf2148e093a15
azmammad.q <azmammad.q@kripton.az>
2022-09-30 17:00:50 +0400

    product pagination is done

--- app.js
+++ app.js
@@ -1,120 +1,117 @@
-const path = require("path");
-const mongoose = require("mongoose");
-const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const MongoDbStore = require("connect-mongodb-session")(session);
-const csrf = require("csurf");
-const flash = require("connect-flash");
-const multer = require("multer");
+const path = require('path');
 
-const errorController = require("./controllers/error");
-const User = require("./models/user");
-const MONGO_URI =
-  "mongodb+srv://azmammad:ylqqNP7wRWq29Tuq@cluster0.6rwjomi.mongodb.net/shop";
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI =
+  'mongodb+srv://azmammad:ylqqNP7wRWq29Tuq@cluster0.6rwjomi.mongodb.net/shop';
 
 const app = express();
-
-const store = new MongoDbStore({
-  uri: MONGO_URI,
-  collection: "sessions",
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images");
+    cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
-  },
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimeType === "image/png") {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
     cb(null, true);
   } else {
     cb(null, false);
   }
 };
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const csrfProtection = csrf();
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single("image"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
-    secret: "my secret",
+    secret: 'my secret',
     resave: false,
     saveUninitialized: false,
-    store: store,
+    store: store
   })
 );
-
 app.use(csrfProtection);
 app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
-    .then((user) => {
+    .then(user => {
       if (!user) {
         return next();
       }
       req.user = user;
       next();
     })
-    .catch((err) => {
+    .catch(err => {
       next(new Error(err));
     });
 });
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.user;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.use("/500", errorController.get500);
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.status(500).render("500", {
-    pageTitle: "Server Error",
-    path: "/500",
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
   });
 });
 
 mongoose
-  .connect(MONGO_URI)
-  .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Əzməmməd",
-          email: "ezmemmed@yahoo.com",
-          cart: { items: [] },
-        });
-        user.save();
-      }
-    });
+  .connect(MONGODB_URI)
+  .then(result => {
     console.log("connected!");
     app.listen(3000);
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
